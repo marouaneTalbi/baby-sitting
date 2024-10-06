23d8fa1e8f0fe021ed79ef30667a1ae4632c2025
Marouane <marouane@mbp-de-rui.home>
2024-10-06 02:17:58 +0200

    setup login change backend envirment

--- frontend/src/pages/Connexion/SignIn.js
+++ frontend/src/pages/Connexion/SignIn.js
@@ -1,7 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import bgImage from '../../assets/images/img.webp';
+import { ToastContainer, toast } from 'react-toastify';
+import { Button, Card, Label, TextInput } from 'flowbite-react';
+import sendRequest from '../../services/aixosRequestFunction';
+import axios from 'axios';
 
 const SignIn = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const login = async (email, password) => {
+    try {
+        return sendRequest(
+            '/api/login',
+            'post',
+            {
+                email: email,
+                password: password,
+            },
+             false
+        ).then((response) => {
+          console.log(response)
+            if(response) {
+
+              console.log(')))',response)
+                // setToken(response.token)
+                // setUser(response);
+                localStorage.setItem('token', response.token);
+                localStorage.setItem('refresh_token', response.refresh_token);
+                localStorage.setItem('user', JSON.stringify(response));
+                return true;
+            } else {
+                return false;
+            }
+        })
+    } catch (error) {
+        console.log('===>',error)
+    }
+};
+
+  const handleSubmit = async (e) => {
+      e.preventDefault();
+      // setIsLoading(true);
+      try {
+          const success = await login(email, password);
+          // if (success) {
+          //     toast.success('Vous êtes Connecté !');
+          //     // setIsLoading(false);
+          //     // navigate("/");
+          // }
+      } catch (error) {
+          // setIsLoading(false);
+          // toast.error("Username ou mot de passe incorrect");
+      }
+  };
+
   return (
     <div className="min-h-screen flex">
       <div className="w-1/2 bg-cover bg-center" style={{ backgroundImage: `url(${bgImage})` }}>
@@ -9,34 +66,30 @@ const SignIn = () => {
       <div className="w-1/2 flex items-center justify-center p-8 bg-gray-100">
         <div className="w-full max-w-md">
           <h2 className="text-4xl font-bold mb-8 text-center">Sign In</h2>
-          <form className="space-y-6">
+          <form className="space-y-6" onSubmit={handleSubmit}>
             <div>
               <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
               <input
-                type="email"
-                name="email"
-                id="email"
+                id="email" type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required 
                 className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-                required
               />
             </div>
             <div>
               <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
               <input
-                type="password"
-                name="password"
-                id="password"
+                id="password" type="password" placeholder="Mot de passe" value={password} onChange={(e) => setPassword(e.target.value)}
                 className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
                 required
               />
             </div>
             <div>
-              <button
+              {/* <button
                 type="submit"
                 className="w-full bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700"
               >
                 Sign In
-              </button>
+              </button> */}
+              <Button className="w-full bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700" type="submit" color="blue" disabled={isLoading}>{isLoading ? 'Connexion...' : 'Connexion'}</Button>
             </div>
             <div className="text-center">
               <a href="/forgot-password" className="text-sm text-blue-600 hover:underline">
---
+++ frontend/src/services/aixosRequestFunction.js
@@ -0,0 +1,120 @@
+
+import axios from 'axios';
+import { jwtDecode } from "jwt-decode";
+import { useNavigate, Params } from 'react-router';
+// const API_BASE_URL = process.env.REACT_APP_SERVER
+const API_BASE_URL = "http://localhost:8888"
+
+const axiosInstance = axios.create({
+  baseURL: API_BASE_URL
+}); 
+
+const urlsWithoutAuth = ['/api/token/refresh', '/api/users', '/api/mdpresetemail', '/api/resetmdp', '/auth'];
+
+export function getUserRole() {
+  if(!localStorage.getItem('token')) {
+    return null;
+  } else {
+    const token = localStorage.getItem('token');
+    const decodedToken = jwtDecode(token);
+    return decodedToken;
+  }
+}
+
+const sendRequest = async (endpoint, method = 'GET', data = {}, requireAuth = true, params = {}) => {
+  const isValidToken = !isTokenExpired();
+
+  if (requireAuth && isValidToken) {
+    const token = localStorage.getItem('token');
+    if (token) {
+      axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    } else {
+      delete axiosInstance.defaults.headers.common['Authorization'];
+    }
+    try {
+      const response = await axiosInstance({
+        url: endpoint,
+        method,
+        data,
+        params,
+      });
+      return response.data;
+    } catch (error) {
+      console.error('Error with the request:', error.response?.data || error.message);
+      throw error;
+    }
+  } else {
+    try {
+      const response = await axiosInstance({
+        url: endpoint,
+        method,
+        data,
+        params,
+      });
+      return response.data;
+    } catch (error) {
+      console.error('Error with the request:', error.response?.data || error.message);
+      throw error;
+    }
+  }
+}
+
+ 
+axiosInstance.interceptors.request.use(async config => {
+
+  const token = localStorage.getItem('token');
+  const isValidToken = !isTokenExpired();
+
+  if(!token && !urlsWithoutAuth.includes(config.url)) {
+    window.location.replace('/login');
+  }
+
+  if (config.url.includes('/api/login')
+      || (config.url.includes('/api/users') && config.method === 'post')
+  ) {
+
+    return config;
+  }
+
+  if (!isValidToken) {
+    const newToken = await refreshToken();
+    config.headers['Authorization'] = `Bearer ${newToken}`;
+  } else {
+    config.headers['Authorization'] = `Bearer ${token}`;
+  }
+
+  return config;
+}, error => {
+  return Promise.reject(error);
+});
+
+export  function isTokenExpired() {
+  if(!localStorage.getItem('token')) {
+    return null;
+  } else {
+    const token = localStorage.getItem('token');
+
+    const decodedToken = jwtDecode(token);
+    const expirationDate = new Date(decodedToken.exp * 1000);
+    const now = new Date();
+    return now > expirationDate;
+  }
+}
+
+async function refreshToken() {
+  try {
+    const refresh_token = localStorage.getItem('refresh_token');
+    const response = await sendRequest('/api/token/refresh ','post', { refresh_token }, false);
+    localStorage.setItem('token', response.token);
+    if(response) {
+      window.location.reload();
+      return response.token;
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+ 
+
+
+export default sendRequest;
